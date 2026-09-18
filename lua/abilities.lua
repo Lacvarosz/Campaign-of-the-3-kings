4db20dd0cefc259c3fdb3c121f99094816e87350
Homanyi Laszlo <homanyi.laszlo@ssm.hu>
2026-09-18 17:11:39 +0200

    optimization

--- lua/abilities.lua
+++ lua/abilities.lua
@@ -27,8 +27,8 @@ function wesnoth.wml_actions.quick_on_my_feet(cfg)
         end
     elseif mode == "moved" then
         local unit = wesnoth.units.get(unit_id)
-        if not unit then
-            wesnoth.log("warning", "quick_on_my_feet: nem található egység ezzel az ID-vel: " .. tostring(unit_id))
+        if not unit or not M.include(unit.abilities, "quick_on_my_feet") then
+            wesnoth.log("warning", "quick_on_my_feet: nem található egység ezzel az ID-vel és képességgel: " .. tostring(unit_id))
             return
         end
 
@@ -45,39 +45,20 @@ function wesnoth.wml_actions.quick_on_my_feet(cfg)
         unit.variables['qomf_distance'] = dist
 
         if diff > 0 then
-
-            for i = 1, diff do
-                unit:add_modification("object", {
-                    duration = "turn",
-                    silent = "yes",
-                    { "effect", {
-                        apply_to = "defense",
-                        replace = "no",
-                        { "defense", {
-                            deep_water = -5,
-                            shallow_water = -5,
-                            reef = -5,
-                            swamp_water = -5,
-                            flat = -5,
-                            sand = -5,
-                            forest = -5,
-                            hills = -5,
-                            mountains = -5,
-                            village = -5,
-                            castle = -5,
-                            cave = -5,
-                            frozen = -5,
-                            unwalkable = -5,
-                            impassable = -5,
-                            fungus = -5
-                        }}
-                    }},
-                    { "effect", {
-                        apply_to = "attack",
-                        increase_damage = 1
-                    }}
-                })
-            end
+            unit:add_modification("object", {
+                duration = "turn",
+                silent = "yes",
+                { "effect", {
+                    apply_to = "defense",
+                    replace = "no",
+                    increase = -5*diff,
+                    defense_min = 10
+                }},
+                { "effect", {
+                    apply_to = "attack",
+                    increase_damage = 1*diff
+                }}
+            })
         end
         unit.variables['qomf_start_x'] = unit.x
         unit.variables['qomf_start_y'] = unit.y
